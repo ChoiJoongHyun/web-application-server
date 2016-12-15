a2c6201cb97a03ff29abefd390f64aac4783b87c
JoongHyun Choi <joonghyun@JoongHyunui-MacBook-Pro.local>
2016-12-15 20:44:54 +0900

    commit

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,14 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,8 +28,22 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+        	//in 클라이언트에서 데이터가 들어오는것 out 서버가 내보내는것.
+        	log.debug("start--------------------------------------------");
+        	
+        	BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        	String line = br.readLine();
+        	log.debug("requst line : {} " , line);	//첫번째 라인만 보자. GET /index.html HTTP/1.1
+        	if(line == null) {
+        		return ;
+        	}
+        	log.debug("end--------------------------------------------");
+        	
+        	String url = getUrl(line);
+        	
+        	
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
@@ -51,5 +69,9 @@ public class RequestHandler extends Thread {
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+    
+    private String getUrl(String url) {
+    	return url.split(" ")[1];
     }
 }
