b72bbef713ca9ed132c180ace0d7612244520d41
JoongHyun Choi <joonghyun@JoongHyunui-MacBook-Pro.local>
2016-12-15 22:13:35 +0900

    commit

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -9,9 +9,15 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import db.DataBase;
+import model.User;
+import util.HttpRequestUtils;
+import util.IOUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -31,16 +37,54 @@ public class RequestHandler extends Thread {
         	//in 클라이언트에서 데이터가 들어오는것 out 서버가 내보내는것.
         	log.debug("start--------------------------------------------");
         	
-        	BufferedReader br = new BufferedReader(new InputStreamReader(in));
-        	String line = br.readLine();
-        	log.debug("requst line : {} " , line);	//첫번째 라인만 보자. GET /index.html HTTP/1.1
-        	if(line == null) {
-        		return ;
+        	String requestUrl = null;
+        	int contentLength = 0;
+        	
+        	
+        	BufferedReader rd = new BufferedReader(new InputStreamReader(in));
+        	String line;
+        	StringBuffer response = new StringBuffer();
+        	
+        	int i=0;
+        	while((line = rd.readLine()) != null) {
+        		log.debug("line : {} : " + line);
+        		
+        		if(i == 0) {
+        			requestUrl = line;
+        		}
+        		
+        		if(line.startsWith("Content-Length")) {
+        			String[] values = line.split(":");
+        			contentLength = Integer.parseInt(values[1].trim());
+        		}
+        		
+        		response.append(line);
+           	 	response.append('\r');
+        		if (line.equals("") || line == null) {
+        			break;
+        		}
+        		
+        		i++;
         	}
+        	
+        	
+        	log.debug("response toString : " + response.toString());
+        	
         	log.debug("end--------------------------------------------");
+
+        	log.debug("requestUrl : " + requestUrl);
+        	log.debug("contentLength : " + contentLength);
+        	String url = getUrl(requestUrl);
         	
-        	String url = getUrl(line);
-        	
+        	if(requestUrl.startsWith("POST")) {
+        		if(url.contains("/user/create")) {
+            		postUserCreate(url, rd, contentLength);
+            	}
+        	} else if (requestUrl.startsWith("GET")) {
+        		if(url.contains("/user/create")) {
+            		getUserCreate(url);
+            	}
+        	}
         	
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
@@ -70,8 +114,41 @@ public class RequestHandler extends Thread {
             log.error(e.getMessage());
         }
     }
+    private void postUserCreate(String url, BufferedReader rd, int contentLength) {
+    	try {
+			String queryString = IOUtils.readData(rd, contentLength);
+			Map<String, String> params = HttpRequestUtils.parseQueryString(queryString);
+	    	log.debug("params toString : " + params.toString());
+	    	
+	    	//final MyPojo pojo = mapper.map(map, MyPojo.class);
+	    	User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
+	    	DataBase.addUser(user);
+	    	log.debug("user toString : " + user.toString());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+    
+    private void getUserCreate(String url) {
+    	int index = url.indexOf("?");
+    	String requestPath = url.substring(0, index);	// /user/create
+    	String queryString = url.substring(index + 1);	//userId=1&password=2&name=&email=
+    	Map<String, String> params = HttpRequestUtils.parseQueryString(queryString);
+    	    	
+    	//final MyPojo pojo = mapper.map(map, MyPojo.class);
+    	User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
+    	DataBase.addUser(user);
+    	log.debug("user toString : " + user.toString());
+    }
+    
+    
+    
+    
+    
     
     private String getUrl(String url) {
+    	//log.debug("getUrl url : " + url);
     	return url.split(" ")[1];
     }
+    
 }
